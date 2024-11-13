75b5b0c25082785e4f5796283de77a907d809757
matt k <matt.kovach91@gmail.com>
2024-11-13 12:02:54 -0600

    2nd commit

--- api/router.js
+++ api/router.js
@@ -21,4 +21,16 @@ router.get("/:id", (req, res, next) => {
   }
 });
 
+router.post("/", (req, res) => {
+  const { name } = req.body;
+
+  if(!name || name.trim().length === 0) {
+    return res.status(400).json({ message: "A name is required"})
+  }
+  const newId = employees.length ? Math.max(...employees.map(e => e.id)) + 1 : 1;
+  const newEmployee = { id: newId, name };
+  employees.push(newEmployee);
+  res.status(201).json(newEmployee);
+})
+
 module.exports = router;
